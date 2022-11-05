d631ac4a914c0efc62c14d8dfc339ca0b2da2d40
Margo <98500532+brownmar24@users.noreply.github.com>
2022-11-05 16:21:42 -0700

    Trying to merge w/ main

--- Bullet.gd
+++ Bullet.gd
@@ -4,6 +4,7 @@ extends KinematicBody2D
 var shoot = false
 var speed = 400.0
 onready var target = position
+var damage = 1
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,10 +13,12 @@ func _ready():
 	#set_as_toplevel(true)
 
 
-
 func _physics_process(delta):
+	var collision
 	if shoot:
-		move_and_collide(target * speed * delta)
-		
+		collision = move_and_collide(target * speed * delta)
+		if(collision):
+			collision.get_collider().hit(damage)
+			self.queue_free()
 		
 #		apply_impulse(target, Vector2(0, speed))
